3cffe1ab8dcffb03e4980b2a5b1044e94bd730b4
yvikasofficial <yvikasofficial@gmail.com>
2021-01-21 01:57:46 +0530

    dynamic content

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,11 +1,38 @@
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
 import Layout from "../components/layout"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            html
+            excerpt
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here!</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => {
+          return (
+            <li>
+              <h1>{edge.node.frontmatter.title}</h1>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
